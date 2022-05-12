a11eb61d04b02cd6de4eb352d52efa0bd321a38c
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:59 -0300

    chore: re index github-alaviss-union

--- flake.nix
+++ flake.nix
@@ -7,6 +7,30 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."union-main".dir   = "main";
+  inputs."union-main".owner = "nim-nix-pkgs";
+  inputs."union-main".ref   = "master";
+  inputs."union-main".repo  = "union";
+  inputs."union-main".type  = "github";
+  inputs."union-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."union-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."union-0_1_0".dir   = "0_1_0";
+  inputs."union-0_1_0".owner = "nim-nix-pkgs";
+  inputs."union-0_1_0".ref   = "master";
+  inputs."union-0_1_0".repo  = "union";
+  inputs."union-0_1_0".type  = "github";
+  inputs."union-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."union-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."union-0_1_1".dir   = "0_1_1";
+  inputs."union-0_1_1".owner = "nim-nix-pkgs";
+  inputs."union-0_1_1".ref   = "master";
+  inputs."union-0_1_1".repo  = "union";
+  inputs."union-0_1_1".type  = "github";
+  inputs."union-0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."union-0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
